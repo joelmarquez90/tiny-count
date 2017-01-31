3eb98296b8aaca61d32e5ce7cc051f0a1442a8ef
Joel Marquez <jmarquez@monits.com>
2017-01-31 15:57:25 -0300

    Working

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
 var recursive = require('recursive-readdir');
 var tinify = require('tinify');
+var path = require('path');
+var program = require('commander');
 
-tinify.key = '';
+program
+  .version('0.0.1')
+  .option('-k, --key', 'Tinify API Key')
+  .option('-p, --path', 'Images root directory')
+  .parse(process.argv);
 
-recursive('/Users/jmarquez/workspace/mp/cellphonerecharge/Pod/Resources/MPCellphoneRecharge.xcassets', function (err, files) {
+if (program.key) {
+  tinify.key = program.args[0];
+} else {
+  console.log('Tinify API Key is required');
+  process.exit(1);
+}
+
+var rootPath = '';
+if (program.path) {
+  rootPath = program.args[1];
+} else {
+  console.log('Images root directory required');
+  process.exit(1);
+}
+
+var imageExtensions = ['.png', '.jpg', '.jpeg'];
+
+recursive(rootPath, function (err, files) {
   files.forEach(function(file) {
-    tinify.fromFile(file).toFile(file);
+    var fileExtension = path.extname(file);
+    if (imageExtensions.indexOf(fileExtension) > -1) {
+      tinify.fromFile(file).toFile(file);
+    }
   });
 });
