de722bc367f69cf5e28b79b83ce6c82e9290b2c7
Joel Marquez <jmarquez@monits.com>
2017-01-31 17:27:36 -0300

    Documenting

--- index.js
+++ index.js
@@ -29,18 +29,19 @@ if (program.path) {
   process.exit(1);
 }
 
-var imageExtensions = ['.png', '.jpg', '.jpeg'];
-
 // Do the magic!
 recursive(rootPath, function (err, files) {
-  var imageFiles = files.filter(function (file) {
-    var fileExtension = path.extname(file);
-    return imageExtensions.indexOf(fileExtension) > -1;
-  });
 
+  var imageExtensions = ['.png', '.jpg', '.jpeg'];
+
+  // We filter the files by type
+  var imageFiles = files.filter(function (file) { return imageExtensions.indexOf(path.extname(file)) > -1; });
+
+  // Get the current files size
   var filesSize = getFilesSize(imageFiles);
   console.log('Files size before Tinify proccessing: ' + filesize(filesSize));
 
+  // Start tinifying the files
   var imagesProcessed = 0;
   imageFiles.forEach(function (file) {
     tinify.fromFile(file).toFile(file, function(err) {
@@ -49,6 +50,8 @@ recursive(rootPath, function (err, files) {
       } else {
         imagesProcessed++;
         if (imagesProcessed === imageFiles.length) {
+
+          // After all, get the size after tinifying
           var filesSize = getFilesSize(imageFiles);
           console.log('Files size after Tinify proccessing: ' + filesize(filesSize));
         }
@@ -57,6 +60,7 @@ recursive(rootPath, function (err, files) {
   });
 });
 
+// Returns the total size of an array of files
 var getFilesSize = function(files) {
   return files.map(function (file) { return fs.statSync(file).size / 1000; })
               .reduce(function (size, total) { return size + total; }, 0);
